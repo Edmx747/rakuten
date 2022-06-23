6f6bc91b77114d5b945be1aeb8d1a9c8777fbd3a
Demircan Erol <edemircan@adista.fr>
2022-06-23 10:58:11 +0200

    fix(htpp_interceptor): standby

--- src/config/http-interceptor.service.ts
+++ src/config/http-interceptor.service.ts
@@ -9,13 +9,6 @@ export class HttpInterceptorService implements HttpInterceptor {
 
   constructor() { }
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    return next.handle(this.addOpenWeatherKey(request));
-  }
-  addOpenWeatherKey(request: HttpRequest<any>): HttpRequest<any> {
-    let newParams = new HttpParams({fromString: request.params.toString()});
-    newParams = newParams.append('appid', 'b166f3f5572f0c211c8245beda46b0e4');
-    return request.clone({
-      params: newParams
-    });
+    return next.handle(request);
   }
 }
