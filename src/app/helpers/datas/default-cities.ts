65a59e71858efb6c8beafde44b3f2823b6cdb1f1
Demircan Erol <edemircan@adista.fr>
2022-06-23 10:56:29 +0200

    feat(history): add history and session storage

---
+++ src/app/helpers/datas/default-cities.ts
@@ -0,0 +1,8 @@
+export const defaultCities =
+[
+  { name: 'Paris', selected: false },
+  { name: 'London', selected: false },
+  { name: 'New York', selected: false },
+  { name: 'Tokyo', selected: false },
+  { name: 'Berlin', selected: false },
+]
---
+++ src/app/models/citiesHistory.ts
@@ -0,0 +1,4 @@
+export interface CitiesHistory {
+  name: string;
+  selected: boolean
+}
